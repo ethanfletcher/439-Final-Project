523a6c8b9eb61ff2a55be3477be89c64be3d264a
mjcameron <macauleycameron@gmail.com>
2016-05-24 12:07:41 -0700

    Add files via upload
    not working

--- panic_room_button.ino
+++ panic_room_button.ino
@@ -1,36 +1,33 @@
 #include<Servo.h>
-int pos = 0;
+int pos = 90;
 
 Servo servo;
 int directionState = 0;
+int buttonState = 0;
+
+const int trapDoorBtn = 8;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
+  pinMode(8, INPUT);
   servo.attach(9);
 }
 void loop() {
   servo.write(pos);
-  if (directionState == 0){
-    if (digitalRead(2) == HIGH) {
-      directionState = 1;
+  buttonState = digitalRead(trapDoorBtn);
+  
+  if (buttonState == HIGH) {
       for (pos=0; pos < 90; pos=pos+1); {
         Serial.println(pos);
         servo.write(pos); 
         delay(15); 
-        Serial.println(directionState);                             
-    }
-    }
-  }
-    else if (directionState == 1) {
-      if (digitalRead(2) == HIGH) {
-        directionState = 0;
-        for (pos=90; pos>=1; pos=pos-1); {
-          Serial.println(pos);
-          servo.write(pos); 
-          delay(15); 
-          Serial.println(directionState);           
+       // Serial.println(directionState);                          
+      }
+      for (pos=90; pos>=1; pos=pos-1); {
+        Serial.println(pos);
+        servo.write(pos); 
+        delay(15); 
+       // Serial.println(directionState);           
      }
   }
-  }
-  }
+}

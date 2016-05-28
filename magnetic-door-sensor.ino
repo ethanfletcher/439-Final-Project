782f1e637322123e4a79007b4ab83ab0163346d6
ethanfletcher <ethanfletcher95@gmail.com>
2016-05-28 13:54:07 -0700

    Add files via upload
    partial code w/ millis()

---
+++ magnetic-door-sensor.ino
@@ -0,0 +1,14 @@
+  runTime = millis();
+  if(digitalRead(magneticSwitchPin) == HIGH & magneticDirectionState == 0) {
+    digitalWrite(ledPin, HIGH);
+    if (millis() >= (runTime + 5000)) {
+      magneticDirectionState = 1;
+    }
+  }
+  runTime = millis();
+  if (digitalRead(magneticSwitchPin) == HIGH & magneticDirectionState == 1){
+    digitalWrite(ledPin, LOW);
+    if (millis() >= (runTime + 5000)) {
+      magneticDirectionState = 0;
+    }
+  }

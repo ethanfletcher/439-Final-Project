4a950865643be86df2318a69c0fa4f27633e45bb
mjcameron <macauleycameron@gmail.com>
2016-05-28 15:14:17 -0700

    Update magnetic-door-sensor.ino

--- magnetic-door-sensor.ino
+++ magnetic-door-sensor.ino
@@ -1,14 +1,34 @@
-  runTime = millis();
+/* #include <SmartCityNodeLib.h>
+
+http://www.instructables.com/id/Magnetic-Door-Sensor-and-Arduino/?ALLSTEPS
+
+*/
+
+const int magneticSwitchPin = 5;
+const int ledPin = 13;
+int magneticDirectionState = 0;
+int ledState = LOW;
+
+void setup() {
+pinMode(magneticSwitchPin, INPUT);
+pinMode(ledPin, OUTPUT);
+digitalWrite(magneticSwitchPin, HIGH);
+digitalWrite(ledPin, LOW);
+}
+
+void loop() {
   if(digitalRead(magneticSwitchPin) == HIGH & magneticDirectionState == 0) {
+    ledState = HIGH;
     digitalWrite(ledPin, HIGH);
-    if (millis() >= (runTime + 5000)) {
-      magneticDirectionState = 1;
-    }
   }
-  runTime = millis();
-  if (digitalRead(magneticSwitchPin) == HIGH & magneticDirectionState == 1){
+  if (digitalRead(magneticSwitchPin) == LOW & ledState == HIGH) {
+    magneticDirectionState = 1;
+  }
+  if(digitalRead(magneticSwitchPin) == HIGH & magneticDirectionState == 1) {
+    ledState = LOW;
     digitalWrite(ledPin, LOW);
-    if (millis() >= (runTime + 5000)) {
-      magneticDirectionState = 0;
-    }
   }
+  if (digitalRead(magneticSwitchPin) == LOW & ledState == LOW) {
+    magneticDirectionState = 0;
+  }
+}
